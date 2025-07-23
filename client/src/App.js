2dbde9e698e5ebdc733698c32bcdbe542cbf2ae3
CSEHema <hemasiri1405@gmail.com>
2025-07-23 17:39:35 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
+import Navbar from './Components/Navbar';
+import Registration from './Components/Registration';
 
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Registration/>} />
+      </Routes>
+    </Router>
+    </>
+    
+  );
+}
+
+export default App;
